1d2b714e61bd359198ca92eb8b6c7a6400b78cdf
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2018-08-03 10:06:28 +0200

    Added Aria role to breadcrumb and map

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -190,9 +190,10 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.getElementById('breadcrumblist');
   const li = document.createElement('li');
   li.className = "breadcrumbElements";
+  li.setAttribute('arria-current', "page")
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 }
