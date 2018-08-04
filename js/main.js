43bdc3677f53194e6f77d264a70ad73abdbfcd2f
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2018-08-04 12:51:55 +0200

    Corrected map visibility

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1IjoiYmluaWNvZGVzIiwiYSI6ImNqam8zOWdqNjA5Nm4zcHFkejBiOHRrZnMifQ.fWe7nz3oB8X_Ud9PuiETFg>',
+    mapboxToken: 'pk.eyJ1IjoiYmluaWNvZGVzIiwiYSI6ImNqam8zOWdqNjA5Nm4zcHFkejBiOHRrZnMifQ.fWe7nz3oB8X_Ud9PuiETFg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
